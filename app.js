1d27e0b1b63633ee120f82ebf8cf1aea1818b98e
youheiishiguro <youhei.ishiguro@gmail.com>
2022-04-15 23:15:26 +0900

    Manual Review OK

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Good Morning!!'));
+app.get('/', (req, res) => res.send('Manual Review OK!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
